1985f75ea5219cd824ec31bf81951ab93ff91f83
Ville Karvonen <ville.karvonen@raute.com>
2026-02-18 10:23:31 +0200

    ask for notification permission

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.composetutorial
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,12 +29,16 @@ object Conversation
 data class Profile(val name: String)
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission())
+    {
+        isGranted: Boolean -> if (isGranted) {startSensorService()}
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val serviceIntent = Intent(this, SensorService::class.java)
-        startForegroundService(serviceIntent)
+        checkAndRequestPermission()
 
         val avatarFile = File(filesDir, "profilePic.jpg")
         if (!avatarFile.exists()) {
@@ -45,6 +54,25 @@ class MainActivity : ComponentActivity() {
                 MyApp()
             }
         }
+    }
+
+    private fun checkAndRequestPermission() {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED -> {
+                    startSensorService()
+                }
+                ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.POST_NOTIFICATIONS) -> {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+    }
+
+    private fun startSensorService() {
+        val serviceIntent = Intent(this, SensorService::class.java)
+        ContextCompat.startForegroundService(this, serviceIntent)
     }
 }
 
